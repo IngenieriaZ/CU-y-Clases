f58a3d282ce9f782f9e0fb4f7ba47f36dadec263
Tatis Vivas <t_vivas@javeriana.edu.co>
2024-05-29 01:17:17 +0000

    casos uso

---
+++ docs/README.md
@@ -0,0 +1,5 @@
+# Casos de uso 
+
+En el la siguiente imagen se encuentra el diagrama de los 5 casos de uso básicos para el funcionamiento de la parte inicial de WinBici
+
+![alt text](image-5.png)
--- objetivo/src/main/java/com/bicicletas/objetivo/dataAccess/ObjetivoRepository.java
+++ objetivo/src/main/java/com/bicicletas/objetivo/dataAccess/ObjetivoRepository.java
@@ -11,11 +11,6 @@ import com.bicicletas.objetivo.modelo.Objetivo;
 public interface ObjetivoRepository
     extends JpaRepository<Objetivo, UUID>
 {
-// obtiene el objetivo con "enProceso == true"")
-/*
-// obtiene los trayectos entre fecha inico y fecha fin
-@Modifying
-@Query("select t from Objetivos t where t.horaFin > :horaInicio and t.horaInicio < :horaFin")
-List<Trayecto> findEnRangoFechas(Date horaInicio, Date horaFin);
-*/
+
+    //falta esto todo
 }
