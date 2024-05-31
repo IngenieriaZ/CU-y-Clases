5cb45bb7a4071e789cab40f1daffe1d5c15cbea6
Daniel Leon <dafelepe10@gmail.com>
2024-05-31 01:26:28 +0000

    html

--- objetivo/src/main/java/com/bicicletas/objetivo/Saludo.java
+++ objetivo/src/main/java/com/bicicletas/objetivo/Saludo.java
@@ -1,4 +1,16 @@
 package com.bicicletas.objetivo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/app")
 
+public class Saludo{
+
+    @GetMapping()
+    public String hola(){
+        return "hola";
+    }
+}
